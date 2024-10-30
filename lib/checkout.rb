8a9d72daece49ef5f22d957139590748b34c91f3
Gary Y <garyyau080@gmail.com>
2024-10-30 14:16:10 +0900

    chore: Add checklist and comments pertaining to refactoring of Checkout class

--- lib/checkout.rb
+++ lib/checkout.rb
@@ -1,7 +1,22 @@
+# Tasks and specifications
+# - Look for instances where code can be refactored to be easier to maintain
+# - Add tests that might be needed and let it guide development using TTD
+# - Add new requirements as specified, eg. Mangoes: buy 3 get 1 free
+# - SOLID principles where applicable
+
+# - Single Responsibility
+# - Open/Closed
+# - Liskov Substitution
+
+# Last two may not be as pertinent depending on progress
+# - Interface Separation
+# - Dependency Inversion
+
 class Checkout
   attr_reader :prices
   private :prices
 
+  #  Basket and separate discount logic can be put into the initialize
   def initialize(prices)
     @prices = prices
   end
@@ -10,6 +25,7 @@ class Checkout
     basket << item.to_sym
   end
 
+  # This total has too many responsibilities and the nested ifs make it harder to maintain. Separate classes for discount logic?
   def total
     total = 0
 
@@ -37,6 +53,7 @@ class Checkout
 
   private
 
+  # A Hash with a default would be a nicer data structure
   def basket
     @basket ||= Array.new
   end
