6b40dcd41e1e226efc268b631a49d49dc6c236ff
Gary Y <garyyau080@gmail.com>
2024-10-30 15:09:09 +0900

    test: Add two for one test

--- spec/half_price_spec.rb
+++ spec/half_price_spec.rb
@@ -3,7 +3,7 @@ require 'half_price'
 
 RSpec.describe HalfPrice do
   context 'when a half price offer is applied' do
-    it 'returns discount price' do
+    it 'returns the discounted price' do
       expect(HalfPrice.new.apply(:bananas, 1, {bananas: 30})).to eq(15)
     end
   end
