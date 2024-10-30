d62bbea6a6b1f0311cc316a9e3635d551f889281
Gary Y <garyyau080@gmail.com>
2024-10-30 15:01:37 +0900

    test: Add test for half price building on from no_discount

---
+++ spec/half_price_spec.rb
@@ -0,0 +1,10 @@
+require 'spec_helper'
+require 'half_price'
+
+RSpec.describe HalfPrice do
+  context 'when a half price offer is applied' do
+    it 'returns discount price' do
+      expect(HalfPrice.new.apply(:bananas, 1, {bananas: 30})).to eq(15)
+    end
+  end
+end
