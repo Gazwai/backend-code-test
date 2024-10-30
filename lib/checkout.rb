c2d40d2a2ee2a35d07c4bab0d00f6f9f11f15afe
Gary Y <garyyau080@gmail.com>
2024-10-30 15:45:12 +0900

    doc: Add checkmarks and remove old comments

--- lib/checkout.rb
+++ lib/checkout.rb
@@ -1,11 +1,11 @@
 # TODO: Tasks and specifications
-# - Look for instances where code can be refactored to be easier to maintain
-# - Add tests that might be needed and let it guide development using TTD
-# - Add new requirements as specified, eg. Mangoes: buy 3 get 1 free
-# - SOLID principles where applicable
+# [x] Look for instances where code can be refactored to be easier to maintain
+# [x] Add tests that might be needed and let it guide development using TTD
+# [x] Add new requirements as specified, eg. Mangoes: buy 3 get 1 free
+# [ ] SOLID principles where applicable
 
-# - Single Responsibility
-# - Open/Closed
+# [x] Single Responsibility
+# [x] Open/Closed
 # - Liskov Substitution
 
 # Last two may not be as pertinent depending on progress
@@ -22,7 +22,6 @@ class Checkout
   attr_reader :prices, :basket
   private :prices, :basket
 
-  #  TODO: Basket and separate discount logic can be put into the initialize
   def initialize(prices)
     @prices = prices
     @basket = Hash.new(0)
@@ -39,7 +38,6 @@ class Checkout
     basket << item.to_sym += 1
   end
 
-  # TODO: This total has too many responsibilities and the nested ifs make it harder to maintain. Separate classes for discount logic?
   def total
     basket.sum do |item, count|
       discount = @discounts.fetch(item, NoDiscount.new)
