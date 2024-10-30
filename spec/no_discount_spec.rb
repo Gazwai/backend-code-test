1b6bd80c7f739b65fb3908aeaaf564564c287796
Gary Y <garyyau080@gmail.com>
2024-10-30 14:49:56 +0900

    test: add basic no_discount test

---
+++ spec/no_discount_spec.rb
@@ -0,0 +1,10 @@
+require 'spec_helper'
+require 'no_discount'
+
+RSpec.describe NoDiscount do
+  context 'when no discounts are applied' do
+    it 'returns full price' do
+      pending 'returns full price'
+    end
+  end
+end
