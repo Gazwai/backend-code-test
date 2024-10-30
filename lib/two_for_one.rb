bd786ff670c568901724f9125240f45a05bea14e
Gary Y <garyyau080@gmail.com>
2024-10-30 15:10:11 +0900

    fix: fix pluralisation typos

--- lib/two_for_one.rb
+++ lib/two_for_one.rb
@@ -1,5 +1,5 @@
 class TwoForOne
   def apply(item, count, prices)
-    (count / 2 + count % 2) * price[item]
+    (count / 2 + count % 2) * prices[item]
   end
 end
--- spec/two_for_one_spec.rb
+++ spec/two_for_one_spec.rb
@@ -4,7 +4,7 @@ require 'two_for_one'
 RSpec.describe TwoForOne do
   context 'when a two for one is applied' do
     it 'returns the discounted price' do
-      expect(TwoForOne.new.apply(:apples, 2, {apple: 10})).to eq(10)
+      expect(TwoForOne.new.apply(:apple, 2, {apple: 10})).to eq(10)
     end
   end
 end
