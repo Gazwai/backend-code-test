0d0e91fa7db50241f481cea78812f217a25686cd
Gary Y <garyyau080@gmail.com>
2024-10-30 17:30:33 +0900

    refactor: Add and fetch method for discount db

--- lib/discount_database.rb
+++ lib/discount_database.rb
@@ -1,5 +1,13 @@
 class DiscountDatabase
   def initialize(discounts = {})
-    @discount = discounts
+    @discounts = discounts
+  end
+
+  def add_discount(item, discount)
+    @discounts[item.to_sym] = discount
+  end
+
+  def fecth_discount(item)
+    @discounts.fetch(item.to_sym, NoDiscount.new)
   end
 end
